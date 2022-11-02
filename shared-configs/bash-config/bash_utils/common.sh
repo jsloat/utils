1ca6a343a9c704fd4d7999ca0ccd068214d9c257
John Sloat <jsloat1@gmail.com>
2022-11-01 20:37:54 -0700

    Add util for function params

--- shared-configs/bash-config/bash_utils/common.sh
+++ shared-configs/bash-config/bash_utils/common.sh
@@ -30,3 +30,15 @@ show() {
   *) ls ;;
   esac
 }
+
+_has_param() {
+  local term="$1"
+  shift
+  for arg; do
+    if [[ $arg == "$term" ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+export -f _has_param
